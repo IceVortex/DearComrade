a9ec99b126be00ba9e692aa50365fddbe36a2823
IceVortex <ungureanu.robert97@gmail.com>
2015-04-19 12:47:36 +0300

    Implemented resources, approval and hopefully fixed UnityVS.

---
+++ 1MM_Unity/Assets/Resources/Scripts/GameResources.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameResources : MonoBehaviour 
+{
+    //Here is a private reference only this class can access
+    private static GameResources _instance;
+    public float food, money, buildingMaterials, approval;
+ 
+    //This is the public reference that other classes will use
+    public static GameResources instance
+    {
+        get
+        {
+            //If _instance hasn't been set yet, we grab it from the scene!
+            //This will only happen the first time this reference is used.
+            if(_instance == null)
+                _instance = GameObject.FindObjectOfType<GameResources>();
+            return _instance;
+        }
+    }
+ 
+    public void Play()
+    {
+        //Play some audio!
+    }
+
+}
---
+++ 1MM_Unity/Assets/RobertTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobertTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(GameResources.instance.food);
+        GameResources.instance.food = 10;
+        Debug.Log(GameResources.instance.food);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
