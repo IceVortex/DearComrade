8db9d81cef058d1402dd604d54f6d5419ff14c93
IceVortex <ungureanu.robert97@gmail.com>
2015-04-28 15:21:28 +0300

    End Game Screen, Float decimals and rearranged the project folders.

---
+++ 1MM_Unity/Assets/Resources/Scripts/UI/loggingFrontend.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class loggingFrontend : MonoBehaviour {
+
+    public Color positiveColor, negativeColor;
+    public Text values, approvalValue;
+
+    public void updateValues()
+    {
+        if (GameResources.instance.approval >= 0)
+            approvalValue.color = positiveColor;
+        else
+            approvalValue.color = negativeColor;
+        approvalValue.text = System.Math.Round(GameResources.instance.approval,2).ToString();
+
+        values.text = "Food: " + System.Math.Round(LoggingSystem.Instance.foodGained,2).ToString() + "\n"
+            + "Materials: " + System.Math.Round(LoggingSystem.Instance.materialsGained,2).ToString() + "\n"
+            + "Money: " + System.Math.Round(LoggingSystem.Instance.moneyGained,2).ToString() + "\n"
+            + "Approval: " + System.Math.Round((-LoggingSystem.Instance.baseApprovalLost + LoggingSystem.Instance.approvalGained),2).ToString();
+    }
+}
---
+++ 1MM_Unity/Assets/Resources/Scripts/UI/resourcesUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class resourcesUI : MonoBehaviour {
+
+    public Text food, citizens, bm, money, troops, researchPoints;
+
+	void Update () {
+        food.text = "Food: " + System.Math.Round(GameResources.instance.food,2).ToString();
+        citizens.text = "Citizens: " + System.Math.Round(GameResources.instance.citizens,2).ToString() + "/" + System.Math.Round(GameResources.instance.maximumCitizens,2).ToString();
+        bm.text = "Materials: " + System.Math.Round(GameResources.instance.buildingMaterials,2).ToString();
+        money.text = "Money: " + System.Math.Round(GameResources.instance.money,2).ToString();
+        troops.text = "Troops: " + System.Math.Round(GameResources.instance.troops,2).ToString() + "/" + System.Math.Round(GameResources.instance.maximumTroops,2).ToString();
+        researchPoints.text = "Research Points: " + System.Math.Round(GameResources.instance.researchPoints, 2).ToString();
+    }
+}
