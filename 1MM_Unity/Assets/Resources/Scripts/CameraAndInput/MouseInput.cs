8b4b341f4cda5f818498f853e378eb2e128eeda5
Iulian Petrariu <iulian.petrariu97@gmail.com>
2015-04-28 22:18:19 +0300

    Moving buildings!

--- 1MM_Unity/Assets/Resources/Scripts/CameraAndInput/MouseInput.cs
+++ 1MM_Unity/Assets/Resources/Scripts/CameraAndInput/MouseInput.cs
@@ -10,8 +10,8 @@ public class MouseInput : MonoBehaviour {
     private Ray2D ray;
     public Collider2D clickedOn;
     public Camera cam;
-    public bool comradery;
-    public GameObject comrade1, comrade2;
+    public bool comradery, move;
+    public GameObject comrade1, comrade2, buildingToMove;
 
     public CanvasGroup resultCG;
     public Text result;
@@ -31,9 +31,15 @@ public class MouseInput : MonoBehaviour {
         comrade1 = window.GetComponentInChildren<windowValues>().clickedByGO;
     }
 
+    public void moveBuilding()
+    {
+        move = true;
+        buildingToMove = window.GetComponentInChildren<windowValues>().clickedByGO;
+    }
+
 
 	void Update () {
-        if (!comradery)
+        if (!comradery && !move)
         {
             if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
             {
@@ -65,7 +71,7 @@ public class MouseInput : MonoBehaviour {
                     build.GetComponent<hide>().toggle();
             }
         }
-        else
+        else if(comradery)
         {
             resultCG.alpha = 1;
             resultCG.interactable = true;
@@ -133,6 +139,39 @@ public class MouseInput : MonoBehaviour {
                 comrade1 = comrade2 = null;
             }
 
+        }
+
+        else if (move)
+        {
+            float xPos, yPos;
+            xPos = cam.ScreenToWorldPoint(Input.mousePosition).x;
+            yPos = cam.ScreenToWorldPoint(Input.mousePosition).y;
+
+            resultCG.alpha = 1;
+            result.text = "Click where you want the building to be moved.";
+
+            if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            {
+                buildingToMove.GetComponent<Collider2D>().enabled = false;
+
+                if (!Physics2D.OverlapArea(new Vector2(xPos - 0.6F, yPos - 0.6F),
+                 new Vector2(xPos + 0.6F, yPos + 0.6F)) )
+                {
+                    buildingToMove.transform.position = new Vector3(xPos, yPos, 0);
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        buildingToMove.transform.position = new Vector3(xPos, yPos, 0);
+                        move = false;
+                        result.text = "Succesfully moved building";
+                        buildingToMove.GetComponent<Collider2D>().enabled = true;
+                        if (resultCG.alpha != 0)
+                            Invoke("hideResult", 1.5F);
+                    }
+                }
+
+            }
+            
         }
 
 	}
