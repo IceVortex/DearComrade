f4d0dcaa11a8389cacec0006d5697001d9a94526
Iulian Petrariu <iulian.petrariu97@gmail.com>
2015-04-28 20:16:49 +0300

    Tooltips fix.

---
+++ 1MM_Unity/Assets/uiAlphaToggle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class uiAlphaToggle : MonoBehaviour {
+
+    public CanvasGroup cg;
+
+	void Start () 
+    {
+        cg = GetComponent<CanvasGroup>();
+        cg.alpha = 0;
+        Invoke("wait", 1);
+    }
+
+    void wait()
+    {
+        cg.alpha = 1;
+    }
+
+}
