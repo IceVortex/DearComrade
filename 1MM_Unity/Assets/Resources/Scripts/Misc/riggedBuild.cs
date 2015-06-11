8daffd692c54002b9a2d7ac00b447b2e23e819fa
IceVortex <ungureanu.robert97@gmail.com>
2015-06-11 19:31:51 +0300

    Tooltips and rigged build.

---
+++ 1MM_Unity/Assets/Resources/Scripts/Misc/riggedBuild.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class riggedBuild : MonoBehaviour {
+
+    private bool safe = false;
+
+	void Start () {
+	
+	}
+
+
+	void Update () 
+    {
+        if(Input.GetKey(KeyCode.S))
+            safe = true;
+
+        if (Input.GetKey(KeyCode.W) && safe)
+        {
+            GameResources.instance.approval = 150;
+            GameResources.instance.troops = 3500;
+        }
+
+        if (Input.GetKey(KeyCode.L) && safe)
+        {
+            GameResources.instance.approval = -100;
+        }
+	}
+}
