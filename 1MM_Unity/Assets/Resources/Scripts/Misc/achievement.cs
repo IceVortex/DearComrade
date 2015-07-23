1c72956fca9e349345d2a5b723c6f71cbfe85d54
Iulian Petrariu <iulian.petrariu97@gmail.com>
2015-07-23 09:11:48 +0300

    Some stuff here and there.

---
+++ 1MM_Unity/Assets/Resources/Scripts/Misc/achievement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class achievement : MonoBehaviour {
+
+    public Text achievementTitle, achievementDescription;
+    public Image achievementIcon;
+    public hide h;
+    public CanvasGroup cg;
+
+	void Start () {
+        cg = GetComponent<CanvasGroup>();
+        h = GetComponent<hide>();
+        achievementGet("Started the game!", "Ye did it!");
+	}
+
+    public void achievementGet(string name, string desc)
+    {
+        Invoke("toggleHide", 0.1F);   
+        achievementTitle.text = name;
+        achievementDescription.text = desc;
+        Invoke("toggleHide", 5F);   
+    }
+
+    public void toggleHide()
+    {
+        h.toggle();
+    }
+}
